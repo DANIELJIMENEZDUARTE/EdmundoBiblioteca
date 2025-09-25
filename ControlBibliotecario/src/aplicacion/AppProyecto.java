5d5c5c5e9bb157b58bbaf2a85296cdb0f8178027
malblam <miriam100bazaldua@gmail.com>
2025-09-24 18:54:31 -0700

    version 1.06 Miriam

--- ControlBibliotecario/src/aplicacion/AppProyecto.java
+++ ControlBibliotecario/src/aplicacion/AppProyecto.java
@@ -4,7 +4,7 @@ import gUILayer.PantallaPrincipal;
 
 public class AppProyecto {
     public static void main(String[] args) {
-        PantallaPrincipal ventana = new PantallaPrincipal("Control Bibliotecario para el franco");
+        PantallaPrincipal ventana = new PantallaPrincipal("Control Bibliotecario para el franco y la Miriam xdxdddd");
         ventana.setVisible(true);
     }
 }
