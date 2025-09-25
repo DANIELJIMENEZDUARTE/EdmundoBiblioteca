f8503abcbbf1b935a755d3a93abab1501fef2792
DANIELJIMENEZDUARTE <l23170198@culiacan.tecnm.mx>
2025-09-24 18:26:17 -0700

    version 1.05

--- ControlBibliotecario/src/aplicacion/AppProyecto.java
+++ ControlBibliotecario/src/aplicacion/AppProyecto.java
@@ -4,7 +4,7 @@ import gUILayer.PantallaPrincipal;
 
 public class AppProyecto {
     public static void main(String[] args) {
-        PantallaPrincipal ventana = new PantallaPrincipal("Control Bibliotecario");
+        PantallaPrincipal ventana = new PantallaPrincipal("Control Bibliotecario para el franco");
         ventana.setVisible(true);
     }
 }
